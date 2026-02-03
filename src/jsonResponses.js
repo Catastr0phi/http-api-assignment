8dd88a170943d7e8141a6af96c20da8514f63ef7
catastrophi <wlzang2@gmail.com>
2026-02-03 13:54:19 -0500

    Started xml

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,56 +1,114 @@
-// Common function for sending a json response
-const jsonRespond = (request, response, status, object) => {
-    const content = JSON.stringify(object);
-
-    console.log(content);
+// Common function for sending a response
+const respond = (request, response, status, object, type) => {
+    console.log(object);
 
     response.writeHead(status, {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(content, 'utf8'),
+        'Content-Type': type,
+        'Content-Length': Buffer.byteLength(object, 'utf8'),
     });
 
-    response.write(content);
+    response.write(object);
     response.end();
 }
 
 const success = (request, response) => {
+    if (request.acceptedTypes[0] === 'text/xml') {
+        let successXML = '<response>';
+        successXML = `${successXML} <message>This is a successful response</message>`;
+        successXML = `${successXML} </response>`;
+
+        // return response passing out string and content type
+        return respond(request, response, 200, successXML, 'text/xml');
+    }
+
     const successJSON = {
         message: 'This is a successful response',
     };
 
-    jsonRespond(request, response, 200, successJSON);
+    const successString = JSON.stringify(successJSON);
+    respond(request, response, 200, successString, 'application/json');
 }
 
 const badRequest = (request, response) => {
+
+    if (request.acceptedTypes[0] === 'text/xml') {
+        let code = 200;
+        let badRequestXML = '<response>';
+
+        // Set invalid message and ID
+        if (!request.query.valid || request.query.valid !== 'true') {
+            badRequestXML = `${badRequestXML} <message>Missing valid query parameter set to true</message>`;
+            badRequestXML = `${badRequestXML} <id>badRequest</id>`;
+            code = 400; // Update error code
+        }
+        // Valid message
+        else {
+            badRequestXML = `${badRequestXML} <message>This request has the required parameters</message>`;
+        }
+
+        badRequestXML = `${badRequestXML} </response>`;
+
+        return respond(request, response, code, badRequestXML, 'text/xml');
+    }
+
+
+
     const requestJSON = {
-        message: 'This request has the required paramters',
+        message: 'This request has the required parameters',
     }
 
     // Verify request is valid
     if (!request.query.valid || request.query.valid !== 'true') {
         requestJSON.message = 'Missing valid query parameter set to true';
         requestJSON.id = 'badRequest';
-        return jsonRespond(request, response, 400, requestJSON);
+
+        const requestString = JSON.stringify(requestJSON);
+        return respond(request, response, 400, requestString, 'application/json');
     }
 
     // Request is valid
-    return jsonRespond(request, response, 200, requestJSON);
+    const requestString = JSON.stringify(requestJSON);
+    return respond(request, response, 200, requestString, 'application/json');
 }
 
 const unauthorized = (request, response) => {
+    if (request.acceptedTypes[0] === 'text/xml') {
+        let code = 200;
+        let unauthorizedXML = '<response>';
+
+        // Set invalid message and ID
+        if (!request.query.loggedIn || request.query.loggedIn !== 'yes') {
+            unauthorizedXML = `${unauthorizedXML} <message>Missing loggedIn query parameter set to yes</message>`;
+            unauthorizedXML = `${unauthorizedXML} <id>unauthorized</id>`;
+            code = 401; // Update error code
+        }
+        // Valid message
+        else {
+            unauthorizedXML = `${unauthorizedXML} <message>This request has the required paraemters</message>`;
+        }
+
+        unauthorizedXML = `${unauthorizedXML} </response>`;
+
+        return respond(request, response, code, unauthorizedXML, 'text/xml');
+    }
+
+
     const unauthorizedJSON = {
-        message: 'This request has the required paramters',
+        message: 'This request has the required parameters',
     }
 
     // Verify request is valid
     if (!request.query.loggedIn || request.query.loggedIn !== 'yes') {
         unauthorizedJSON.message = 'Missing loggedIn query parameter set to yes';
         unauthorizedJSON.id = 'unauthorized';
-        return jsonRespond(request, response, 401, unauthorizedJSON);
+
+        const unauthorizedString = JSON.stringify(unauthorizedJSON);
+        return respond(request, response, 401, unauthorizedString, 'application/json');
     }
 
     // Request is valid
-    return jsonRespond(request, response, 200, unauthorizedJSON);
+    const unauthorizedString = JSON.stringify(unauthorizedJSON);
+    return respond(request, response, 200, unauthorizedString, 'application/json');
 }
 
 const forbidden = (request, response) => {
@@ -59,7 +117,8 @@ const forbidden = (request, response) => {
         id: 'forbidden',
     };
 
-    jsonRespond(request, response, 403, forbiddenJSON);
+    const forbiddenString = JSON.stringify(forbiddenJSON);
+    respond(request, response, 403, forbiddenString, 'application/json');
 }
 
 const internal = (request, response) => {
@@ -68,7 +127,8 @@ const internal = (request, response) => {
         id: 'internalError',
     };
 
-    jsonRespond(request, response, 500, internalJSON);
+    const internalString = JSON.stringify(internalJSON);
+    respond(request, response, 500, internalString, 'application/json');
 }
 
 const notImplemented = (request, response) => {
@@ -77,7 +137,8 @@ const notImplemented = (request, response) => {
         id: 'notImplemented',
     };
 
-    jsonRespond(request, response, 501, notImplementedJSON);
+    const notImplementedString = JSON.stringify(notImplementedJSON)
+    respond(request, response, 501, notImplementedString, 'application/json');
 }
 
 const notFound = (request, response) => {
@@ -86,7 +147,8 @@ const notFound = (request, response) => {
         id: 'notFound',
     };
 
-    jsonRespond(request, response, 404, notFoundJSON);
+    const notFoundString = JSON.stringify(notFoundJSON);
+    respond(request, response, 404, notFoundString, 'application/json');
 }
 
 module.exports = {
